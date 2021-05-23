9e839ab7fb59abb15433e29675f8737a0640de01
lyj0309 <2457395722@qq.com>
2021-05-23 10:28:55 +0800

    1

---
+++ controller/user.go
@@ -0,0 +1,11 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+func Login(c *gin.Context) {
+
+}
+
+func SignIn(c *gin.Context) {
+
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module pgd
+
+go 1.16
+
+require (
+	github.com/appleboy/gin-jwt/v2 v2.6.4
+	github.com/gin-gonic/gin v1.7.1
+)
